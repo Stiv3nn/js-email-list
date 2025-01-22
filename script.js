111e9079d608c9ea96565c0ba7355a2c24ab23c7
Stiven hoxha <stivenhoxha007@gmail.com>
2025-01-22 18:05:41 +0100

    output di un email

--- script.js
+++ script.js
@@ -2,3 +2,27 @@
 // https://flynn.boolean.careers/exercises/api/random/mail
 // generare 10 indirizzi email e stamparli in pagina all’interno di una lista.
  
+
+// Seleziono l'elemento di output
+const outputEmail = document.getElementById('email-list');
+//console.log(outputEmail);
+
+const endpoint = 'https://flynn.boolean.careers/exercises/api/random/mail';
+
+// Faccio partire la richiesta Ajax verso l'API per ricevere l'email
+axios.get(endpoint)
+    .then(response => {
+        // codice da eseguire in caso di successo
+        const result = response.data;
+        let email = result.response;
+        console.log(email);
+        
+        // Output in pagina
+        outputEmail.innerHTML = email;
+    })
+    .catch(error => {
+        // codice da eseguire in caso di errore
+        console.error(error)
+    })
+
+
